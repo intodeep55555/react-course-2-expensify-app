e648543d7ee3b58b731f1bb04342eaad04cb97f5
Li <Shu@Lis-MacBook-Pro.local>
2018-12-08 20:06:06 +0900

    Remove dummy expense data

--- src/index.js
+++ src/index.js
@@ -14,14 +14,6 @@ import getVisibleExpenses from './selectors/expenses';
 import configureStore from './store/configureStore';
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount:4500}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 //This is for providing react props with redux created store.
 //Then, connect them like what 'ExpenseList.js' does.
 const jsx = (
